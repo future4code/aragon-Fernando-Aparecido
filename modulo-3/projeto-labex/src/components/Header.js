cf74bfca2412983d84856e1fc53d375615388b87
fernando467 <101422627+fernando467@users.noreply.github.com>
2022-05-25 16:03:09 -0300

    projeto-labex

--- modulo-3/projeto-labex/src/components/Header.js
+++ modulo-3/projeto-labex/src/components/Header.js
@@ -1,9 +1,36 @@
-import {useNavigate} from "react-router-dom"
+import axios from "axios"
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
 function Header() {
+  const [email, setEmail] = useState("")
+
+  const [senha, setSenha] = useState("")
   const navigate = useNavigate()
+  const mudaEmail = (e) => { setEmail(e.target.value) }
+  const mudaSenha = (e) => { setSenha(e.target.value) }
+  const logar = () => {
+    const body = {
+      email: email,
+      password: senha
+    }
+    axios.post("https://us-central1-labenu-apis.cloudfunctions.net/labeX/fernando-aragon/login", body)
+      .then((res) => { 
+        localStorage.setItem("token-labex", res.data.token)
+       alert("logado com sucesso!")
+        navigate("/admin")
+      })
+      .catch((err) => { alert(err.response.data) })
+  }
   return <header>
-        <h1>Bem-Vindo ao: Labex. </h1>
-        <button onClick={()=>navigate("/admin")}>entrar</button>
-    </header>
+    <h1>Bem-Vindo ao: Labex. </h1>
+    <form>
+      <label htmlFor="email">e-mail:</label>
+      <input id="email" value={email} onChange={mudaEmail} />
+      <br></br>
+      <label htmlFor="senha">senha:</label>
+      <input id="senha" value={senha} onChange={mudaSenha} />
+    </form>
+    <button onClick={logar}>entrar</button>
+  </header>
 }
 export default Header
--- modulo-3/projeto-labex/src/paginas/PaginaAdmin.js
+++ modulo-3/projeto-labex/src/paginas/PaginaAdmin.js
@@ -1,9 +1,18 @@
-import Header from "../components/Header"
+import { useNavigate } from "react-router-dom"
 
-function PaginaAdmin () {
-    return <section>
-        <Header/>
-        <h1>Bem-Vindo Admin.</h1>
+
+function PaginaAdmin() {
+  const navigate = useNavigate()
+  const logout =()=>{
+      localStorage.removeItem("token-labex")
+      navigate("/")
+      
+  }
+
+   return <section>
+<h1>Labe-x</h1>
+        <button onClick={logout}>sair</button> 
+        <h3>Bem-Vindo Admin.</h3>
     </section>
 }
 export default PaginaAdmin
