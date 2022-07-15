65f6ff317f90c0196d5e275e37557df20564ab5f
fernando467 <101422627+fernando467@users.noreply.github.com>
2022-07-15 12:57:50 -0300

    projeto finalizado

--- modulo4/back-end/node-package-jason/exercicio1/exercicio1.js
+++
@@ -1,5 +0,0 @@
-//process.argv[2...]
-//1.B
-console.log(`olá, eu sou o ${process.argv[2]}e tenho ${process.argv[3]} anos`)
-
-//1C
--- modulo4/back-end/node-package-jason/exercicio2/exercicio2.js
+++
@@ -1,3 +0,0 @@
-console.log(`soma: ${Number(process.argv[2]) + Number(process.argv[3])}`)
-
-console.log(`subitração: ${Number(process.argv[2]) - Number(process.argv[3])}`)
