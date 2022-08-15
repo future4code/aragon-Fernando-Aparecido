1a192f0caa27fc4ff73a594f188ca802de61b462
fernando467 <101422627+fernando467@users.noreply.github.com>
2022-08-15 17:34:59 -0300

    exercicio do dia

---
+++ modulo5/teste-jeste/tests/postbusiness/createPost.test.ts
@@ -0,0 +1,5 @@
+import {PostBusiness } from "../../src/business/PostBusiness"
+import { PostDatabase } from "../../src/database/PostDatabase"
+describe("testando post business",() => {
+const postBusiness= new PostBusiness(new PostDatabase ) 
+})
